dabe8e76b2e69d757d80e9a5bf7dd6367a8f48a8
Rohit Katlaa <42460632+rohitkatlaa@users.noreply.github.com>
2018-11-25 18:48:22 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,14 @@
+import os
+os.system("pip3 install flask")
+os.system("pip3 install flask-wtf")
+os.system("pip3 install flask-sqlalchemy")
+os.system("pip3 install flask-migrate")
+os.system("pip3 install flask-login")
+os.system("pip3 install flask-bootstrap")
+os.system("pip3 install flask-moment")
+os.system("pip3 install Pillow")
+os.system("flask db init")
+os.system("flask db migrate")
+os.system("flask db upgrade")
+os.system("flask run")
+
